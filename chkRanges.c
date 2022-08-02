9393ad7c968164ff54d33ce694ea7921dfa04fb8
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-08-02 17:11:06 +0530

    Update chkRanges.c

--- chkRanges.c
+++ chkRanges.c
@@ -2,12 +2,44 @@
 
 int ranges = 0;
 int chrgSession[] = {3, 3, 5, 4, 10, 11, 12};
-int sizeOfchrgSess = sizeof(chrgSession) / sizeof(chrgSession[0]);
 int rangeCount[3] = {0};
 
 void chkrange() {
+    int sizeOfchrgSess = sizeof(chrgSession) / sizeof(chrgSession[0]);
+    sortRange(chrgSession, 0, sizeOfchrgSess);
     checkRange(chrgSession,sizeOfchrgSess);
 }
+
+void swapValues(int* value1, int* value2) 
+{ 
+    int temp = *value1; 
+    *value1 = *value2;
+    *value2 = temp; 
+}   
+
+int partition (int array[], int lowIndex, int highIndex) 
+{ 
+    int pivotElement = array[highIndex];
+    int i = (lowIndex - 1);
+    for (int index = lowIndex; index <= highIndex- 1; index++) {
+        if (array[index] <= pivotElement) {
+            i++;
+            swapValues(&array[i], &array[index]);
+        }
+    }
+    swapValues(&array[i + 1], &array[highIndex]);
+    return (i + 1);
+} 
+
+void sortRange(int array[], int lowIndex, int highIndex)
+{  
+    if (lowIndex < highIndex) {
+        int pivot = partition(array, lowIndex, highIndex);
+        // Separately sort elements before & after partition
+        quickSort(array, lowIndex, pivot - 1);
+        quickSort(array, pivot + 1, highIndex);
+    }
+} 
 
 int *checkRange(int *array, int size)
 {  
@@ -28,6 +60,6 @@ void printRange()
 {  
     int index;
     for(index=0; index<=ranges; index++) {
-            printf("count is %d\n", rangeCount[index]);
+            printf("count[%d] is %d\n", index, rangeCount[index]);
     }
 }
