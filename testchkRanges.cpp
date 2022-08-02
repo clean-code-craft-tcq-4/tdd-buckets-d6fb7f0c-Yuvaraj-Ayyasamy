090125298584f8f333673cbc23a66a35b5a2af62
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-08-02 18:59:51 +0530

    Update testchkRanges.cpp

--- testchkRanges.cpp
+++ testchkRanges.cpp
@@ -7,8 +7,6 @@ int main()
     int testArraySize = sizeof(testArray)/sizeof(testArray[0]);
     int testArray1[] = {30, 50, 40, 20};
     int testArray1Size = sizeof(testArray1)/sizeof(testArray1[0]);
-    int chargeSession[] = {3, 3, 5, 4, 10, 11, 12, 11};
-    int chargeSessionSize = sizeof(chargeSession)/sizeof(chargeSession[0]);
 
     swapValues(&testValue1, &testValue2);
     assert((testValue1==20) && (testValue2==10));
@@ -21,15 +19,10 @@ int main()
     assert((testArray1[0]==20) && (testArray1[1]==30) && (testArray1[2]==40) && (testArray1[3]==50));
     //printRange(testArray, testArraySize-1);
 
-    //checkRange(chargeSession, chargeSessionSize);
-    //assert(rangeCount[0] == 4);
-    //assert(rangeCount[1] == 4);
-    //printRange(rangeCount, 3);
-
     chkrange();
-    //assert(rangeCount[0] == 4);
-    //assert(rangeCount[1] == 3);
-    //assert(rangeCount[2] == 1);
-    printRange(rangeCount, 3);
+    assert(rangeCount[0] == 4);
+    assert(rangeCount[1] == 3);
+    assert(rangeCount[2] == 1);
+    //printRange(rangeCount, 3);
     return 0;
 }
