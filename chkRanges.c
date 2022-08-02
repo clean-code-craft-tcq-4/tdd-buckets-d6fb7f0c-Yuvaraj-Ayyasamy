3cd95171de36cb8e139bb82a9715f28d60a311d3
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-08-02 17:32:12 +0530

    Update chkRanges.c

--- chkRanges.c
+++ chkRanges.c
@@ -2,7 +2,7 @@
 
 int ranges = 0;
 int chrgSession[] = {3, 3, 5, 4, 10, 11, 12};
-int rangeCount[3] = {0};
+int rangeCount[3] = {1};
 
 void chkrange() {
     int sizeOfchrgSess = sizeof(chrgSession) / sizeof(chrgSession[0]);
@@ -48,7 +48,7 @@ int *checkRange(int array[], int size)
     int index;
     for(index=0; index<size; index++) {
         if ((array[index+1] == (array[index] + 1)) || (array[index+1] == array[index])) {
-            rangeCount[ranges] = rangeCount[ranges]+1;
+            rangeCount[ranges] += 1;
         }
         else {
             ranges++;
