8c27df7ec35e6f5f9dcbd82c1a85e76dd1f85486
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-08-02 17:14:31 +0530

    Update chkRanges.h

--- chkRanges.h
+++ chkRanges.h
@@ -6,3 +6,6 @@ extern int rangeCount[];
 void chkrange();
 int *checkRange(int *array, int size);
 void printRange();
+void swapValues(int* value1, int* value2);
+int partition(int array[], int lowIndex, int highIndex);
+void sortRange(int array[], int lowIndex, int highIndex);
