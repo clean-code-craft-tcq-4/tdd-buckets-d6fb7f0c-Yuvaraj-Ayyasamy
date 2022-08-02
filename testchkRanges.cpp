0d7a7779533ace3aedd1a484008aea7979472bad
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-08-02 18:13:35 +0530

    Update testchkRanges.cpp

--- testchkRanges.cpp
+++ testchkRanges.cpp
@@ -5,14 +5,18 @@ int main()
     int testValue1 = 10, testValue2 = 20;
     int testArray[] = {30, 10, 20};
     int testArraySize = sizeof(testArray)/sizeof(testArray[0]);
-    
+
     swapValues(&testValue1, &testValue2);
     assert((testValue1==20) && (testValue2==10));
-    
-    partition(testArray, 0, testArraySize);
+
+    partition(testArray, 0, testArraySize-1);
+    //assert((testArray[0]==10) && (testArray[1]==20) && (testArray[2]==30));
+    printRange(testArray, testArraySize-2);
+
+    partition(sortRange, 0, testArraySize);
     //assert((testArray[0]==10) && (testArray[1]==20) && (testArray[2]==30));
     printRange(testArray, testArraySize-1);
-    
+
     chkrange();
     assert(rangeCount[0] == 4);
     assert(rangeCount[1] == 3);
