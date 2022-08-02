4c827e1879aebd80d87e2c681c0d5ac647919a5d
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-08-02 15:54:35 +0530

    Update chkRanges.c

--- chkRanges.c
+++ chkRanges.c
@@ -1,5 +1,10 @@
 #include "chkRanges.h"
 
+int ranges = 0;
+int chrgSession[] = {3, 3, 5, 4, 10, 11, 12};
+int sizeOfchrgSess = sizeof(chrgSession) / sizeof(chrgSession[0]);
+int rangeCount[3] = {0};
+
 int *chkrange() {
     return checkRange(chrgSession,sizeOfchrgSess);
 }
