09858ba5f5db41092b09286b6b9668f5afaacbac
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-08-02 17:37:58 +0530

    Update chkRanges.c

--- chkRanges.c
+++ chkRanges.c
@@ -2,7 +2,7 @@
 
 int ranges = 0;
 int chrgSession[] = {3, 3, 5, 4, 10, 11, 12};
-int rangeCount[3] = {1};
+int rangeCount[3] = {1, 1, 1};
 
 void chkrange() {
     int sizeOfchrgSess = sizeof(chrgSession) / sizeof(chrgSession[0]);
