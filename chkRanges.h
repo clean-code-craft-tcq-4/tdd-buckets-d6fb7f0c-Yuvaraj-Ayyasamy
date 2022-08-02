40dd2c8dcd69fb87bd84695eb0555cb30bdd536c
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-08-02 15:54:25 +0530

    Update chkRanges.h

--- chkRanges.h
+++ chkRanges.h
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 
-int rangeCount[3] = {0};
-int ranges = 0;
-int chrgSession[] = {3, 3, 5, 4, 10, 11, 12};
-int sizeOfchrgSess = sizeof(chrgSession) / sizeof(chrgSession[0]);
+extern int rangeCount[];
 
 int *chkrange();
 int *checkRange(int *array, int size);
