ce8b53b71046b7a8462a58242a7ce1fb0aba17c2
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-08-02 18:21:46 +0530

    Update testchkRanges.cpp

--- testchkRanges.cpp
+++ testchkRanges.cpp
@@ -11,7 +11,7 @@ int main()
 
     partition(testArray, 0, testArraySize-1);
     //assert((testArray[0]==10) && (testArray[1]==30));
-    printRange(testArray, testArraySize-3);
+    printRange(testArray, testArraySize-1);
 
     sortRange(testArray, 0, testArraySize);
     assert((testArray[0]==10) && (testArray[1]==20) && (testArray[2]==30) && (testArray[3]==40));
