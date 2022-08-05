52989ddef8d601275fb207d123bc2b59a681b3bf
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-08-05 15:21:40 +0530

    Update chkRanges.h

--- chkRanges.h
+++ chkRanges.h
@@ -9,5 +9,6 @@ void chkrange();
 void checkRange(int array[], int size);
 void printRange(int array[], int size);
 void swapValues(int* value1, int* value2);
+void addInExcel(int array[], int size);
 int partition(int array[], int lowIndex, int highIndex);
 void sortRange(int array[], int lowIndex, int highIndex);
