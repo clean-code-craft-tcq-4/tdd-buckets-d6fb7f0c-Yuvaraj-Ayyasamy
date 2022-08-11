7f93157c41e280f697a7b838c80d74c110663e91
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-08-11 23:16:53 +0530

    Update testchkRanges.cpp

--- testchkRanges.cpp
+++ testchkRanges.cpp
@@ -3,7 +3,8 @@ void testswapValues()
 {
     int testValue1 = 10, testValue2 = 20;
     swapValues(&testValue1, &testValue2);
-    assert((testValue1==20) && (testValue2==10));
+    assert(testValue1==20);
+    assert(testValue2==10);
 }
 
 void testpartition()
@@ -11,7 +12,8 @@ void testpartition()
     int testArray[] = {30, 10, 20};
     int testArraySize = sizeof(testArray)/sizeof(testArray[0]);
     partition(testArray, 0, testArraySize-1);
-    assert((testArray[0]==10) && (testArray[1]==20));
+    assert(testArray[0]==10);
+    assert(testArray[1]==20);
 }
 
 void testsortRange()
@@ -19,7 +21,9 @@ void testsortRange()
     int testArray1[] = {30, 50, 20};
     int testArray1Size = sizeof(testArray1)/sizeof(testArray1[0]);
     sortRange(testArray1, 0, testArray1Size-1);
-    assert((testArray1[0]==20) && (testArray1[1]==30) && (testArray1[2]==50));
+    assert(testArray1[0]==20);
+    assert(testArray1[1]==30);
+    assert(testArray1[2]==50);
 }
 
 void testcompareRange()
@@ -29,13 +33,15 @@ void testcompareRange()
     assert(compareRange(CurPosVal, IncPosVal, (CurPosVal+1)) == true);
     CurPosVal = 2;
     IncPosVal = 5;
-    assert(compareRange(CurPosVal, IncPosVal, (CurPosVal+1)) == FALSE);
+    assert(compareRange(CurPosVal, IncPosVal, (CurPosVal+1)) == false);
 }
 
 void testcheckrange()
 {
     checkrange();
-    assert((rangeCount[0] == 4) && (rangeCount[1] == 3) && (rangeCount[2] == 1));
+    assert(rangeCount[0] == 4);
+    assert(rangeCount[1] == 3);
+    assert(rangeCount[2] == 1);
 }
 
 void testcountRange()
