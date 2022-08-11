27b74e89b3557a6627792da8bc083e419007baaa
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-08-11 20:34:43 +0530

    Update chkRanges.h

--- chkRanges.h
+++ chkRanges.h
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <assert.h>
 
+struct data
+{
+    int id;
+    char range[10];
+    int count;
+}s;
+
 extern int rangeCount[];
 extern int chrgSession[];
 extern int rangeCount[];
 
-void chkrange();
-void checkRange(int array[], int size);
+void checkrange();
+void countRange(int array[], int size);
 void printRange(int array[], int size);
 void swapValues(int* value1, int* value2);
 void addInExcel(int array[]);
