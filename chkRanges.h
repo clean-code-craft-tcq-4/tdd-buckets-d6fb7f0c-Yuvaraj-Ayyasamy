4776662c4c0f04fdadb8a899ea1c73e381b39324
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-02 11:49:47 +0530

    Update chkRanges.h

--- chkRanges.h
+++ chkRanges.h
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <assert.h>
 
-extern int rangeCount[];
 extern int chrgSession[];
 
 void checkrange();
-void countRange(int array[], int size);
+void countRange(int array[], int rangeCount[], int size);
 void printRange(int array[], int size);
 void swapValues(int* value1, int* value2);
 bool compareRange(int CurPosVal, int IncPosVal, int CurPosIncVal);
