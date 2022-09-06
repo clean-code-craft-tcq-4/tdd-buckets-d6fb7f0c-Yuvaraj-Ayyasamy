eacde18938417516c07bffc9a483a13aa78b5863
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-06 15:55:04 +0530

    Update A2DConverter.h

--- A2DConverter.h
+++ A2DConverter.h
@@ -2,20 +2,26 @@
 #include <stdio.h>
 #include <math.h>
 
+typedef enum {
+    TwelveBitADC = 12,
+    TenBitADC = 10,
+}ADCTypes;
+
+typedef enum {
+    ToController = 0,
+    ToMail = 1,
+}AlertTypes;
+
+#define AMPARRAYSIZE          7
 #define INSIDE_RANGE          1
 #define OUTOFF_RANGE          0
 #define minRangeADC(ADCType)  0
 #define maxRangeADC(ADCType)  (pow(2, ADCType)-2)
 #define AMPRANGE              10
 
-#define TWELVEBITMAXRANGE     (pow(2, 12)-2)
+#define TWELVEBITMAXRANGE     (pow(2, TwelveBitADC)-2)
 #define TENBITMINRANGE        0
-#define TENBITMAXRANGE        (pow(2, 10)-2)
-
-enum ADCType {
-	TwelveBitADC = 12,
-	TenBitADC = 10,
-};
+#define TENBITMAXRANGE        (pow(2, TenBitADC)-2)
 
 #define RECEPIENT        "a.b@c.com"
 #define ERROR_MSG        "Error: ADC out off Range:"
@@ -29,11 +35,13 @@ enum ADCType {
     printf("To: %s\n Hi, %s %d !!!\n", RECEPIENT, ERROR_MSG, A2DConvertData);  \
 })
 
-int ADCRangeCheck(int ADCData, int ADCType);
-int A2D12bitConverter(int ADCData, int ADCType);
-int A2D10bitConverter(int ADCData, int ADCType);
-int A2DConverter(int ADCData, int ADCType);
-void find_convertedAmpere_RangeList(int convertedData);
+void (*alertType[2]) (int A2DConvertData);
+int ADCRangeCheck(int ADCData, ADCTypes ADCType);
+int A2D12bitConverter(int ADCData, ADCTypes ADCType);
+int A2D10bitConverter(int ADCData, ADCTypes ADCType);
+int A2DConverter(int ADCData, ADCTypes ADCType);
+int createAmpRangeList(int A2DConvertData);
 void sendToController(int A2DConvertData);
 void sendToEmail(int A2DConvertData);
-bool processADCSensorData(int ADCData, int ADCType, int A2DConvertData, void (*alertType) (int A2DConvertData));
+void alarmOutOfRange(int A2DConvertData, AlertTypes alertType);
+bool processADCSensorData(int ADCData, ADCTypes ADCType, int A2DConvertData, AlertTypes alertType);
