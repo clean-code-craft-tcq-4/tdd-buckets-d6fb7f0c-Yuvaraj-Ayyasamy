5cb2dacf3b65c645953e8e34800de37ad6e99a12
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-06 16:49:43 +0530

    Update chkRanges.h

--- chkRanges.h
+++ chkRanges.h
@@ -8,7 +8,7 @@ extern int ampList[];
 int AddCurrentAmpList(int A2DConvertData);
 void checkRange(int chrgSession[], int sizeOfchrgSess, int rangeCount[], int sizeOfRngCnt);
 void countRange(int array[], int rangeCount[], int size);
-void printRange(int array[], int size);
+void printRange(int chrgSession[], int array[], int size);
 void swapValues(int* value1, int* value2);
 bool compareRange(int CurPosVal, int IncPosVal, int CurPosIncVal);
 int partition(int array[], int lowIndex, int highIndex);
