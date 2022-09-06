c1f083bbd950e014346702f5cc0fd0cc17e40023
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-06 14:01:30 +0530

    Update A2DConverter.h

--- A2DConverter.h
+++ A2DConverter.h
@@ -8,23 +8,23 @@
 #define maxRangeADC(ADCType)  (pow(2, ADCType)-2)
 #define AMPRANGE              10
 
-#define 12BITMAXRANGE (pow(2, 12)-2)
-#define 10BITMINRANGE 0
-#define 10BITMAXRANGE (pow(2, 10)-2)
+#define TWELVEBITMAXRANGE     (pow(2, 12)-2)
+#define TENBITMINRANGE        0
+#define TENBITMAXRANGE        (pow(2, 10)-2)
 
 enum ADCType {
-	12BitADC = 12;
-	10BitADC = 10,
+	TwelveBitADC = 12,
+	TenBitADC = 10,
 };
 
 #define RECEPIENT        "a.b@c.com"
 #define ERROR_MSG        "Error: ADC out off Range:"
-#define THROW_CONTROLLERWARNING(int A2DConvertData)                            \
+#define THROW_CONTROLLERWARNING(A2DConvertData)                                \
 ({                                                                             \
-    printf(""%s %d!!!\n", ERROR_MSG, A2DConvertData);                          \
+    printf("%s %d!!!\n", ERROR_MSG, A2DConvertData);                           \
 })
 
-#define THROW_MAILWARNING(int A2DConvertData)                                  \
+#define THROW_MAILWARNING(A2DConvertData)                                      \
 ({                                                                             \
     printf("To: %s\n Hi, %s %d !!!\n", RECEPIENT, ERROR_MSG, A2DConvertData);  \
 })
