0d03a66dd61e0382c5412cd9b0af2eb822ad3bb0
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-06 14:18:24 +0530

    Update A2DConverter.c

--- A2DConverter.c
+++ A2DConverter.c
@@ -1,30 +1,38 @@
 #include "A2DConverter.h"
 
 int ADCRangeCheck(int ADCData, int ADCType) {
-	if(ADCData > minRangeADC(ADCType) && ADCData <= maxRangeADC(ADCType)) 	{
-		return INSIDE_RANGE;
-	} else {
-		return OUTOFF_RANGE;
-	}
+    if (ADCData > minRangeADC(ADCType) && ADCData <= maxRangeADC(ADCType)) {
+	return INSIDE_RANGE;
+    } else {
+	return OUTOFF_RANGE;
+    }
 }
 
 int A2D12bitConverter(int ADCData, int ADCType) {
- 	return round((AMPRANGE * ADCData)/maxRangeADC(ADCType));
+    return round((AMPRANGE * ADCData)/maxRangeADC(ADCType));
 }
 
 int A2D10bitConverter(int ADCData, int ADCType) {
-	int result = round((AMPRANGE * ADCData)/maxRangeADC(ADCType));
- 	if( ADCData < (maxRangeADC(ADCType)/2))
-	    result = 0 - result;
-	return result;
+    int result = round((AMPRANGE * ADCData)/maxRangeADC(ADCType));
+    if (ADCData < (maxRangeADC(ADCType)/2))
+	result = 0 - result;
+    return result;
+}
+
+int A2DConverter(int ADCData, int ADCType) {
+    if (ADCType == TwelveBitADC) {
+        A2D12bitConverter(ADCData, ADCType);
+    } else {
+        A2D10bitConverter(ADCData, ADCType);
+    }
 }
 
 void sendToController(int A2DConvertData) {
-  THROW_CONTROLLERWARNING(A2DConvertData);
+    THROW_CONTROLLERWARNING(A2DConvertData);
 }
 
 void sendToEmail(int A2DConvertData) {
-  THROW_MAILWARNING(A2DConvertData);
+    THROW_MAILWARNING(A2DConvertData);
 }
 
 // void alarmOutOfRange(int A2DConvertData, void (*alertType) (int A2DConvertData)) 
@@ -33,21 +41,21 @@ void sendToEmail(int A2DConvertData) {
 // }
 
 void find_convertedAmpere_RangeList(int convertedData) {
-	int data = convertedData;
-	find_currentRangeList(&data, 1);	
+    int data = convertedData;
+    find_currentRangeList(&data, 1);	
 }
 
 bool processADCSensorData(int ADCData, int ADCType, int A2DConvertData, void (*alertType) (int A2DConvertData)) {
-	int errorStatus;
-	
+    int errorStatus;
+
     errorStatus = ADCRangeCheck(ADCData, ADCType);
-    if(errorStatus == INSIDE_RANGE) {
+    if (errorStatus == INSIDE_RANGE) {
     	A2DConvertData = A2DConverter(ADCData, ADCType);
-		find_convertedAmpere_RangeList(A2DConvertData);
-	} else {
-		A2DConvertData = maxRangeADC(ADCType)+1;
-		// alarmOutOfRange(A2DConvertData);
-		(*alertType) (A2DConvertData);
-	}
-	return errorStatus;
+        find_convertedAmpere_RangeList(A2DConvertData);
+    } else {
+	A2DConvertData = maxRangeADC(ADCType)+1;
+	// alarmOutOfRange(A2DConvertData);
+	(*alertType) (A2DConvertData);
+    }
+    return errorStatus;
 } 
