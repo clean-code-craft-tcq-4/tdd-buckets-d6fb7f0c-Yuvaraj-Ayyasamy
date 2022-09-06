c61bcfe67e05b390e5c6a3f5d73d50df9f3d09ef
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-06 15:38:10 +0530

    Update chkRanges.h

--- chkRanges.h
+++ chkRanges.h
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <assert.h>
 
-extern int chrgSession[];
+//extern int chrgSession[];
+//int chrgSession[] = {3, 3, 5, 4, 10, 11, 12};
+//int rangeCount[3] = {1, 1, 1};
 
-void checkRange();
+void checkRange(int chrgSession[], int rangeCount[]);
 void countRange(int array[], int rangeCount[], int size);
 void printRange(int array[], int size);
 void swapValues(int* value1, int* value2);
