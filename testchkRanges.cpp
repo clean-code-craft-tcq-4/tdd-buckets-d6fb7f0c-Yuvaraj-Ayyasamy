fdb33260986d35b84dfc8852a1474b074fa899d3
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-08 01:34:49 +0530

    Update testchkRanges.cpp

--- testchkRanges.cpp
+++ testchkRanges.cpp
@@ -101,12 +101,9 @@ void testA2D12bitConverter()
 
 void testA2D10bitConverter()
 {
-    A2D10bitConverter(0, TenBitADC);
-    A2D10bitConverter(511, TenBitADC);
-    A2D10bitConverter(1022, TenBitADC);
-    //assert(-15 == A2D10bitConverter(0, TenBitADC));
-    //assert(0 == A2D10bitConverter(511, TenBitADC));
-    //assert(15 == A2D10bitConverter(1022, TenBitADC));
+    assert(-15 == A2D10bitConverter(0, TenBitADC));
+    assert(0 == A2D10bitConverter(511, TenBitADC));
+    assert(15 == A2D10bitConverter(1022, TenBitADC));
 }
 
 void testA2DConverter()
@@ -160,8 +157,8 @@ int main()
     testAddCurrentAmpListOutOfRange();
     testA2D12bitConverter();
     testA2D10bitConverter();
-    //testADCRangeCheck();
-    //testA2DConverter();
-    //testprocessADCSensorData();
+    testADCRangeCheck();
+    testA2DConverter();
+    testprocessADCSensorData();
     return 0;
 }
